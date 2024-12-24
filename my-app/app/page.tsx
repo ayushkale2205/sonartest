d386d905c63216efa3ae9134a0fce5eb74fecf5d
AyushKale0522 <05ayushkale@gmail.com>
2024-12-24 13:27:07 +0530

    testing54

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-export default function Login() {
-  const hello = "why";
+export default function Login() { 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
